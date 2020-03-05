0a3ec1ceb57293a4600ec262419e5e6fc3ef223d
Your Name <you@example.com>
2020-03-05 20:50:38 +0800

    文章展示（下拉）

--- src/views/home/components/com-article.vue
+++ src/views/home/components/com-article.vue
@@ -1,15 +1,18 @@
 <template>
-    <!-- div的作用是给瀑布流区域设置“垂直滚动条”，使得可以进行上拉操作 -->
-    <div class="scroll-wrapper">
+  <!-- div的作用是给瀑布流区域设置“垂直滚动条”，使得可以进行上拉操作 -->
+  <div class="scroll-wrapper">
+    <!-- 下拉包围上拉 -->
+    <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
       <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-      <!-- van-cell单元格组件
+        <!-- van-cell单元格组件
         特点：独占一行
         用法非常灵活、复杂
         title：单元格标题内容
-      -->
+        -->
         <van-cell v-for="item in list" :key="item" :title="item" />
       </van-list>
-    </div>
+    </van-pull-refresh>
+  </div>
 </template>
 
 <script>
@@ -17,13 +20,23 @@ export default {
   name: 'com-article',
   data () {
     return {
-      // 瀑布流相关成员
+      // 下拉成员
+      isLoading: false, // 是否处于加载状态
+      // 上拉相关成员
       list: [], // 数据源
       loading: false, // 瀑布流是否加载处于动画效果
       finished: false // 瀑布流是否停止
     }
   },
   methods: {
+    // 下拉执行的动作
+    onRefresh () {
+      setTimeout(() => {
+        this.onLoad() // 调用上拉获得数据
+        this.isLoading = false // 下拉加载完成/结束加载动画
+        this.$toast.success('刷新成功') // 成功提示
+      }, 1000)
+    },
     onLoad () {
       // 异步更新数据
       // setTimeout 仅做示例，真实场景中一般为 ajax 请求
@@ -46,10 +59,10 @@ export default {
 </script>
 
 <style scoped lang='less'>
-.scroll-wrapper{
-      height: 100%;
-      // 瀑布流区域如果垂直方向内容过多，要呈现滚动条
-      // 是瀑布实现的关键要素
-      overflow-y: auto;
-    }
+.scroll-wrapper {
+  height: 100%;
+  // 瀑布流区域如果垂直方向内容过多，要呈现滚动条
+  // 是瀑布实现的关键要素
+  overflow-y: auto;
+}
 </style>
