2fcc2cfb6d20aa4daed5dc7ce4f0eb2a5e287ba0
Your Name <you@example.com>
2020-03-09 21:27:48 +0800

    文章-我的频道

---
+++ src/views/home/components/com-channel.vue
@@ -0,0 +1,106 @@
+<template>
+  <van-popup
+    :value="value"
+    @input="$emit('input',$event)"
+    closeable
+    close-icon-position="top-left"
+    round
+    position="bottom"
+    :style="{ height: '95%' }"
+  >
+    <div class="channel">
+      <div class="channel-head">
+        <div>
+          <span class="title">我的频道</span>
+          <span class="desc">点击进入频道</span>
+        </div>
+        <div>
+          <van-button type="danger" plain size="mini" round>编辑</van-button>
+        </div>
+      </div>
+      <!--van-grid 没有设置column-num属性，默认是4列-->
+      <van-grid class="channel-content" :gutter="10" clickable>
+        <van-grid-item v-for="(item,index) in channelList" :key="item.id">
+          <span class="text" :style="{color:activeChannelIndex===index?'red':''}">{{item.name}}</span>
+          <!-- <van-icon class="close-icon" name="close" /> -->
+        </van-grid-item>
+      </van-grid>
+    </div>
+
+    <div class="channel">
+      <div class="channel-head">
+        <div>
+          <span class="title">频道推荐</span>
+          <span class="desc">点击添加频道</span>
+        </div>
+      </div>
+      <van-grid class="channel-content" :gutter="10" clickable>
+        <van-grid-item v-for="value in 8" :key="value">
+          <div class="info">
+            <span class="text">文字</span>
+          </div>
+        </van-grid-item>
+      </van-grid>
+    </div>
+  </van-popup>
+</template>
+
+<script>
+export default {
+  name: 'com-channel',
+  props: {
+    value: {
+      type: Boolean,
+      default: false
+    },
+    channelList: {
+      type: Array,
+      default: () => []
+    },
+    activeChannelIndex: {
+      type: Number,
+      default: 0
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.channel {
+  margin-top: 70px;
+  .channel-head {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    .title {
+      font-size: 30px;
+      margin-right: 5px;
+    }
+    .desc {
+      font-size: 16px;
+      color: gray;
+    }
+  }
+  .channel-content {
+    .text {
+      font-size: 16px;
+    }
+    .active {
+      color: red;
+    }
+    .close-icon {
+      font-size: 20px;
+      position: absolute;
+      top: -5px;
+      right: -5px;
+      z-index: 999;
+      background-color: #fff;
+    }
+    .info {
+      display: flex;
+      align-items: center;
+    }
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,25 @@
 <template>
   <div class="container">
     <van-tabs v-model="activeChannelIndex">
+      <div slot="nav-right" class="threeLine">
+        <van-icon name="wap-nav" @click="showPopup=true" />
+      </div>
       <van-tab :title="item.name" v-for="item in channelList" :key="item.id">
         <com-article :channelID="item.id"></com-article>
       </van-tab>
     </van-tabs>
+    <!-- 弹出层 -->
+    <com-channel
+      v-model="showPopup"
+      :channelList="channelList"
+      :activeChannelIndex="activeChannelIndex"
+    ></com-channel>
   </div>
 </template>
 
 <script>
+// 对com-channel.vue 做导入、注册、使用
+import ComChannel from './components/com-channel.vue'
 // 导入频道api
 import { apiUserChannel } from '@/api/channel.js'
 // 对com-article.vue 做导入、注册、使用
@@ -16,10 +27,12 @@ import ComArticle from './components/com-article.vue'
 export default {
   name: 'home-index',
   components: {
-    ComArticle
+    ComArticle,
+    ComChannel
   },
   data () {
     return {
+      showPopup: false, // 控制子组件弹出层是否显示
       activeChannelIndex: 0, // 激活频道下标标志
       channelList: [] // 用户频道列表
     }
@@ -56,5 +69,19 @@ export default {
   /deep/ .van-tabs__line {
     background-color: #1989fa;
   }
+}
+
+/deep/ .van-tabs__wrap {
+  width: 90%;
+}
+.threeLine {
+  position: fixed;
+  right: 10px;
+  font-size: 40px;
+  background-color: white;
+  line-height: 88px;
+  height: 88px;
+  width: 90px;
+  text-align: center;
 }
 </style>
