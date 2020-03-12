33bc2e6d0b3b224ccbbce89921ef72a5535b030c
Your Name <you@example.com>
2020-03-12 19:56:39 +0800

    联想历史记录-页面布局

--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -11,13 +11,34 @@
       @input="serachChange"
       @search="onSearch(searchText)"
     />
-    <van-cell-group>
+    <van-cell-group v-if="suggestionList.length">
       <!-- 联想关键字列表 -->
       <van-cell icon="search" v-for="(item,i) in suggestionList" :key="i" @click="onSearch(item)">
         <!-- 因为要应用methods方法，并且该方法返回的信息里边有 html标签+css样式
         所以不要直接使用title属性，相反要应用命名插槽，内部结合v-html应用-->
         <div slot="title" v-html="highLightCell(item,searchText)"></div>
       </van-cell>
+    </van-cell-group>
+    <van-cell-group v-else>
+      <!-- 历史记录 -->
+      <van-cell title="历史记录">
+        <!-- slot="right-icon" 命名插槽 给 cell单元格的右边显示内容(垃圾桶图标) -->
+        <van-icon
+          name="delete"
+          slot="right-icon"
+          v-show="!isDeleteData"
+          @click="isDeleteData=!isDeleteData"
+          style="line-height:inherit"
+        ></van-icon>
+        <div slot="default" v-show="isDeleteData">
+          <span style="margin-right:10px">全部删除</span>
+          <span @click="isDeleteData=!isDeleteData">完成</span>
+        </div>
+      </van-cell>
+        <!-- 历史联想项目数据展示 -->
+        <van-cell title="hello111">
+          <van-icon v-show="isDeleteData" slot="right-icon" name="close" style="line-height:inherit"></van-icon>
+        </van-cell>
     </van-cell-group>
   </div>
 </template>
@@ -28,6 +49,7 @@ export default {
   name: 'search-index',
   data () {
     return {
+      isDeleteData: false, // 联想历史记录是否进入删除状态
       searchText: '',
       suggestionList: [] // 联想建议数据
     }
