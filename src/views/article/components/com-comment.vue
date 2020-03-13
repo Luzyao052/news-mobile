ea111f83619437c82f2c0b91445656b3c190d181
Your Name <you@example.com>
2020-03-13 21:26:16 +0800

    文章详情-回复的页面布局

--- src/views/article/components/com-comment.vue
+++ src/views/article/components/com-comment.vue
@@ -25,11 +25,23 @@
           <p>
             <span>{{item.pubdate | formatTime}}</span>
             ·
-            <span>{{item.reply_count}}&nbsp;回复</span>
+            <span @click="showReply=true">{{item.reply_count}}&nbsp;回复</span>
           </p>
         </div>
       </van-cell>
     </van-list>
+    <!-- 回复列表展示-弹出层/瀑布 -->
+    <van-popup v-model="showReply" position="bottom" :style="{ height: '80%' }">
+      <!-- 瀑布加载效果 -->
+      <van-list
+        v-model="reply.loading"
+        :finished="reply.finished"
+        finished-text="没有更多了"
+        @load="onLoadReply"
+      >
+        <van-cell v-for="item in reply.list" :key="item" :title="item" />
+      </van-list>
+    </van-popup>
   </div>
 </template>
 
@@ -39,6 +51,13 @@ import { apiCommentList } from '@/api/article.js'
 export default {
   data () {
     return {
+      // 回复瀑布相关成员，通过reply成员包围，使得与外部的评论瀑布成员没有冲突
+      reply: {
+        list: [],
+        loading: false, // 瀑布动画
+        finished: false // 瀑布停止标志
+      },
+      showReply: false,
       commentList: [], // 评论列表
       offset: null, // 评论分页标志
       list: [],
@@ -47,6 +66,25 @@ export default {
     }
   },
   methods: {
+    // 对评论回复的瀑布流
+    onLoadReply () {
+      // 异步更新数据
+      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.reply.list.push(this.reply.list.length + 1)
+        }
+
+        // 加载状态结束
+        this.reply.loading = false
+
+        // 数据全部加载完成
+        if (this.reply.list.length >= 40) {
+          this.reply.finished = true
+        }
+      }, 1000)
+    },
+    // 评论瀑布流
     async onLoad () {
       await this.$sleep(800)
       // 获取评论的传参
@@ -57,7 +95,7 @@ export default {
         limit: 10
       }
       const res = await apiCommentList(obj)
-      console.log(res)
+      // console.log(res)
       if (res.results.length) {
         this.commentList.push(...res.results)
         // 对分页标志offset进行赋值
