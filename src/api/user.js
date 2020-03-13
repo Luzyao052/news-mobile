9311af034263e08e8e9ea6730cff4044096bf4fc
Your Name <you@example.com>
2020-03-13 20:31:10 +0800

    文章详情-关注与取消关注

--- src/api/user.js
+++ src/api/user.js
@@ -6,3 +6,9 @@ import { createAPI } from '@/utils/request.js'
 // }
 // 用户界面登录
 export const apiUserLogin = data => createAPI('app/v1_0/authorizations', 'post', data)
+
+// 关注用户
+export const apiUserFollow = data => createAPI('/app/v1_0/user/followings', 'post', data)
+
+// 取消关注
+export const apiUserUnfollow = data => createAPI(`/app/v1_0/user/followings/${data}`, 'delete')
--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -4,18 +4,18 @@
     <div class="detail">
       <h3 class="title">{{article.title}}</h3>
       <div class="author">
-        <van-image
-          round
-          width="1rem"
-          height="1rem"
-          fit="fill"
-          :src="article.aut_photo"
-        />
+        <van-image round width="1rem" height="1rem" fit="fill" :src="article.aut_photo" />
         <div class="text">
           <p class="name">{{article.aut_name}}</p>
           <p class="time">{{article.pubdate | formatTime}}</p>
         </div>
-        <van-button round size="small" type="info">{{article.is_followed?'取消关注':'+ 关注'}}</van-button>
+        <van-button
+          :loading="followLoading"
+          round
+          size="small"
+          @click="followMe()"
+          type="info"
+        >{{article.is_followed?'取消关注':'+ 关注'}}</van-button>
       </div>
       <!--  -->
       <div class="content">
@@ -37,18 +37,53 @@
 </template>
 
 <script>
+// 关注与取消关注api
+import { apiUserFollow, apiUserUnfollow } from '@/api/user.js'
+// 获取文章详情api
 import { apiArticleDetail } from '@/api/article.js'
 export default {
   name: 'article-index',
   data () {
     return {
-      article: {} // 目标文章详情信息
+      article: {}, // 目标文章详情信息
+      followLoading: false
     }
   },
   created () {
     this.getArticleDetail()
   },
   methods: {
+    // 关注作者
+    async followMe () {
+      // 开启加载动画
+      this.followLoading = true
+      // 延迟时间
+      await this.$sleep(1000)
+      if (this.article.is_followed) {
+        // 取消关注
+        await apiUserUnfollow(this.article.aut_id.toString())
+        // 页面更新数据，使得响应式执行
+        this.article.is_followed = false
+      } else {
+        // 关注
+        // target 后台需要的被关注的用户id
+        try {
+          const target = this.article.aut_id.toString()
+          await apiUserFollow({ target })
+          this.article.is_followed = true // 不会有错误
+        } catch (e) {
+          if (e.response.status === 400) {
+            // return 就是停止后续代码执行
+            this.$toast.fail('自己不能关注自己！')
+          } else {
+            this.$toast.fail('关注失败，请联系管理员')
+          }
+        }
+      }
+      // 回复按钮状态
+      this.followLoading = false
+    },
+    // 获取文章详情
     async getArticleDetail () {
       const articleId = this.$route.params.aid
       const res = await apiArticleDetail(articleId)
@@ -67,25 +102,25 @@ export default {
 }
 .detail {
   padding: 0 20px 88px;
-  margin-top:92px;
+  margin-top: 92px;
   .title {
     font-size: 36px;
     line-height: 2;
   }
-  .zan{
+  .zan {
     text-align: center;
     padding: 20px 0;
-    .active{
-      border-color:red;
+    .active {
+      border-color: red;
       color: red;
     }
   }
   .author {
     padding: 20px 0;
     display: flex;
-    position:sticky;
+    position: sticky;
     background-color: #fff;
-    top:92px;
+    top: 92px;
     z-index: 2;
     .text {
       flex: 1;
@@ -107,14 +142,14 @@ export default {
     overflow: hidden;
     white-space: pre-wrap;
     word-break: break-all;
-    p{
-      font-size:28px;
+    p {
+      font-size: 28px;
     }
-    /deep/ img{
-      max-width:100%;
+    /deep/ img {
+      max-width: 100%;
       background: #f9f9f9;
     }
-    /deep/ code{
+    /deep/ code {
       white-space: pre-wrap;
     }
   }
