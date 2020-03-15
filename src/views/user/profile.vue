55a1e36653be27993114e97ddd983b30e254f1fc
Your Name <you@example.com>
2020-03-15 20:33:02 +0800

    个人中心-编辑资料-子级弹层布局

--- src/views/user/profile.vue
+++ src/views/user/profile.vue
@@ -9,7 +9,7 @@
     ></van-nav-bar>
     <!-- 绘制 头像、名称、性别、生日 的单元格 -->
     <van-cell-group>
-      <van-cell title="头像" is-link center>
+      <van-cell title="头像" is-link center @click="showPhoto=true">
         <van-image
           slot="default"
           round
@@ -19,19 +19,59 @@
           :src="userprofile.photo"
         />
       </van-cell>
-      <van-cell title="名称" is-link :value="userprofile.name"></van-cell>
-      <van-cell title="性别" is-link :value="userprofile.gender===0?'男':'女'"></van-cell>
-      <van-cell title="生日" is-link :value="userprofile.birthday"></van-cell>
+      <van-cell title="名称" is-link :value="userprofile.name" @click="showName=true"></van-cell>
+      <van-cell title="性别" is-link :value="userprofile.gender===0?'男':'女'" @click="showSex=true"></van-cell>
+      <van-cell title="生日" is-link :value="userprofile.birthday" @click="showBirthday=true"></van-cell>
     </van-cell-group>
+    <!-- 头像弹出层
+        高度不配置，通过内容自动填充
+    -->
+    <van-popup v-model="showPhoto" position="bottom">
+      <van-cell title="本地相册选择图片" is-link></van-cell>
+      <van-cell title="拍照" is-link></van-cell>
+    </van-popup>
+    <!-- 名称弹出层
+        高度不配置，通过内容自动填充
+    -->
+    <van-popup v-model="showName" position="bottom" style="height:20%">
+      <!-- 通过v-model把当前用户真实的名字表现出来
+          .trim ： 自动去除左右空格
+      -->
+      <van-field v-model.trim="userprofile.name" type="text"></van-field>
+    </van-popup>
+    <!-- 性别弹出层(上拉菜单)
+    -->
+    <van-action-sheet v-model="showSex" :actions="sexMenus" @select="onSelect" cancel-text="取消" />
+    <!-- 生日弹出层(弹出层+时间选择器) -->
+    <van-popup v-model="showBirthday" position="bottom">
+      <van-datetime-picker
+        v-model="currentDate"
+        type="date"
+        :min-date="minDate"
+        :max-date="maxDate"
+        @confirm="onConfirm"
+        @cancel="showBirthday = false"
+      />
+    </van-popup>
   </div>
 </template>
 
 <script>
+import dayjs from 'dayjs'
 import { apiUserProfile } from '@/api/user.js'
 export default {
   name: 'user-profile',
   data () {
     return {
+      minDate: new Date(1900, 1, 1),
+      maxDate: new Date(2025, 10, 1),
+      currentDate: new Date(),
+      showBirthday: false, // 是否显示生日弹层
+      // 给性别上拉菜单 配置选取项目，语法结构固定，name属性固定
+      sexMenus: [{ name: '男' }, { name: '女' }],
+      showSex: false, // 是否显示性别的上拉菜单
+      showName: false, // 是否显示编辑昵称的弹层
+      showPhoto: false, // 是否显示头像弹层
       userprofile: {}
     }
   },
@@ -39,10 +79,28 @@ export default {
     this.getUserProfile()
   },
   methods: {
+    // 日期
+    onConfirm (val) {
+      // console.log(val)
+      this.userprofile.birthday = dayjs(val).format('YYYY-MM-DD')
+      // 关闭弹出层
+      this.showBirthday = false
+    },
+    // 性别
+    onSelect (item) {
+      // console.log(item) // {name:'男'}
+      // 对item进行处理，然后赋值给data表单成员(其接受数字的性别)
+      this.userprofile.gender = item.name === '男' ? 0 : 1
+      // 隐藏菜单
+      this.showSex = false
+    },
     async getUserProfile () {
       const res = await apiUserProfile()
       // console.log(res)
       this.userprofile = res
+      // 把用户生日转为 new Date()  格式，赋值给 currentDate
+      // 使得选择器 默认显示
+      this.currentDate = new Date(this.userprofile.birthday)
     },
     save () {
       // 提示信息
